bcd317b4a5469c18b3df1ae59384cb4ac62ff350
Dima <ldmenergo@yandex.ru>
2023-03-28 20:23:31 +0500

    add regul_intant

--- index.js
+++ index.js
@@ -39,11 +39,8 @@ function solution(str){
          .split('')
          .reverse()
 }
-
-// Завершите решение, чтобы оно перевернуло все слова в переданной строке.
-
+//  *********************Завершите решение, чтобы оно перевернуло все слова в переданной строке.********************************************///
 // Слова разделены ровно одним пробелом, и нет ни начальных, ни конечных пробелов.
-
 // Пример (ввод --> вывод): "The greatest victory is that which requires no battle" -->
 //  "battle no requires which that is victory greatest The"
 
@@ -55,5 +52,61 @@ function reverseWords(str){
     .join(" ")
      
   }
-//   reverseWords('The greatest victory is that which requires no battle');
-  console.log(reverseWords('The greatest victory is that which requires no battle'));
+//  console.log(reverseWords('The greatest victory is that which requires no battle'));
+
+///***************Завершите функцию/метод, чтобы он возвращал URL-адрес с чем-либо после #удаления привязки ( ).******************* */
+//            Примеры
+//            "www.codewars.com#about" --> "www.codewars.com"
+//            "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+// function removeUrlAnchor(url){
+//   url.split('');
+//   console.log(url);
+//   let result = true;
+//  while (result) {
+//    result = url
+//   console.log("sd;lfjsd;lfj;");
+//  }
+  
+//   return url
+// }
+// removeUrlAnchor("www.codewars.comabout");
+
+// let a = 14;
+// console.log(a.toString(16));
+// console.log(isNaN(a));
+// console.log(isFinite(a));
+// let b = 3.4897;
+// console.log(typeof(+b.toFixed(2)));
+// console.log(parseInt("100px"));
+// console.log(parseFloat("100.5px"));
+// let str  = 'Слово в \'кавычках\', слешь \\ и символ в юникоде \u23F1';
+// console.log(str);
+// console.log(str.codePointAt(0));
+// console.log(str.indexOf('c'));
+// console.log(str.slice(10));
+// console.log(str.substr(10,3));
+// console.log(str.toLocaleLowerCase());
+// console.log(str.trim());
+
+let str1  = 'Переходите на мой лучший сайт http:www.govno.ru - не пожалеете';
+let str2  = ' Спасибо за информацию';
+function isHaveLink (str) {
+  let reg = /https?:\/\/.+?\.[a-z]{2,}\s/im;
+  return str.match(reg);
+}
+if (isHaveLink(str1)) {
+  console.log(str1);
+  console.log('Найдена ссылка в строке');
+}
+if (isHaveLink(str2)) {
+  console.log(str2);
+  console.log('Найдена ссылка в строке');
+}
+
+let money = 'У мня в кармане 150 руб и 200 рублей на карте';
+let reg = /\d+/g;
+let matches = money.match(reg);
+console.log(matches);
+money.replace(reg, 'N');
+
